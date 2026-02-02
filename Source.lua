f2d6a8b6c84377d9b8e8a7a54d6e62741b072d7b
create-stree <kirsiacb@gmail.com>
2026-02-03 00:02:16 +0900

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -160,7 +160,7 @@ function CircleClick(Button, X, Y)
 	spawn(function()
 		Button.ClipsDescendants = true
 		local Circle = Instance.new("ImageLabel")
-		Circle.Image = "rbxassetid://76485989009491"
+		Circle.Image = "rbxassetid://140084565218977"
 		Circle.ImageColor3 = Color3.fromRGB(80, 80, 80)
 		Circle.ImageTransparency = 0.8999999761581421
 		Circle.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
