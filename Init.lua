aec81a0a03a5fca36c6faa7e2b4d2c7bd7b1afb2
create-stree <kirsiacb@gmail.com>
2026-03-14 22:59:08 +0900

    Delete Init.lua

--- Init.lua
+++
@@ -1,9 +0,0 @@
-local Library = {}
-Library.__index = Library
-
-function Library:CreateWindow(config)
-    local Window = require(script.core.window)
-    return Window.new(config)
-end
-
-return Library
